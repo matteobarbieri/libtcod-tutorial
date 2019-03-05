9f9dfd3a2302f2bf1b7c843df8f07ea838f53cd1
Matteo Barbieri <neopard@gmail.com>
2019-03-06 00:18:30 +0100

    Add map object and render walls

--- engine.py
+++ engine.py
@@ -3,11 +3,21 @@ import libtcodpy as libtcod
 from entity import Entity
 from input_handlers import handle_keys
 from render_functions import clear_all, render_all
+from map_objects.game_map import GameMap
+
 
 def main():
 
     screen_width = 80
     screen_height = 50
+
+    map_width = 80
+    map_height = 45
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
 
     # player_x = int(screen_width / 2)
     # player_y = int(screen_height / 2)
@@ -42,6 +52,7 @@ def main():
 
     con = libtcod.console_new(screen_width, screen_height)
 
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -49,7 +60,9 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, screen_width, screen_height)
+        # render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
+
 
         # ACtually print stuff on screen
         libtcod.console_flush()

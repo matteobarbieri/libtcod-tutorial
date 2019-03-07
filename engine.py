259f1a09b3e15483fd3ca74cc91c2ba3c0a671df
Matteo Barbieri <neopard@gmail.com>
2019-03-07 13:21:10 +0100

    Add field of view implementation
    Still shows the whole map though

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ import libtcodpy as libtcod
 
 from entity import Entity
 from input_handlers import handle_keys
+from fov_functions import initialize_fov, recompute_fov
 from render_functions import clear_all, render_all
 from map_objects.game_map import GameMap
 
@@ -18,9 +19,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     # player_x = int(screen_width / 2)
@@ -66,15 +73,29 @@ def main():
             player
     )
 
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        # render_all(con, entities, screen_width, screen_height)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            recompute_fov(
+                    fov_map, 
+                    player.x, 
+                    player.y, 
+                    fov_radius, 
+                    fov_light_walls, 
+                    fov_algorithm
+            )
 
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
 
         # ACtually print stuff on screen
         libtcod.console_flush()
@@ -92,6 +113,8 @@ def main():
 
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+
+                fov_recompute = True
             # player.move(dx, dy)
 
         # if key.vk == libtcod.KEY_ESCAPE:

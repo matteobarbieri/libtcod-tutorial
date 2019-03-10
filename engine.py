92532a959962f0ff73cbc90cbab8e366c5b5fcda
Matteo Barbieri <neopard@gmail.com>
2019-03-10 21:43:49 +0100

    Add comments in engine.py

--- engine.py
+++ engine.py
@@ -13,28 +13,33 @@ from death_functions import kill_monster, kill_player
 
 def main():
 
+    # Size of the game screen
     screen_width = 80
     screen_height = 50
 
-
+    # Size of the panel containing health bar and log
     bar_width = 20
     panel_height = 7
     panel_y = screen_height - panel_height
 
-
+    # Size of the playing map
     map_width = 80
     map_height = 43
 
+    # Parameters for rooms generation
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
 
+    # Variables for field of view (FOV)
     fov_algorithm = 0
     fov_light_walls = True
     fov_radius = 10
 
+    # Maximum number of monsters per room
     max_monsters_per_room = 3
 
+    # Colors for rooms in and out of fov
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150),
@@ -42,6 +47,7 @@ def main():
         'light_ground': libtcod.Color(200, 180, 50)
     }
 
+    # Fighter component for player
     fighter_component = Fighter(hp=30, defense=2, power=5)
 
     # Create the Player object
@@ -58,11 +64,13 @@ def main():
 
     entities = [player]
 
+    # Set font
     libtcod.console_set_custom_font(
             'data/fonts/arial10x10.png', 
             libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD
     )
 
+    # Initialize main window
     libtcod.console_init_root(
             screen_width, 
             screen_height, 
@@ -70,13 +78,13 @@ def main():
             False
     )
 
-
     # The main game screen
     con = libtcod.console_new(screen_width, screen_height)
 
     # The UI screen
     panel = libtcod.console_new(screen_width, panel_height)
 
+    # Create the game map
     game_map = GameMap(map_width, map_height)
     game_map.make_map(
             max_rooms, 
@@ -89,18 +97,26 @@ def main():
             max_monsters_per_room
     )
 
+    # When the game starts the fov must be computed for the first time
     fov_recompute = True
 
     fov_map = initialize_fov(game_map)
 
+    # Handles for keyboard and mouse inputs
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
+    # Begin the game in player's turn
     game_state = GameStates.PLAYERS_TURN
 
+    # Main loop
     while not libtcod.console_is_window_closed():
+
+        # Wait for an event
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
+        # If the fov needs to be recomputed (for instance, because the player
+        # moved, do it
         if fov_recompute:
             recompute_fov(
                     fov_map, 
@@ -111,10 +127,12 @@ def main():
                     fov_algorithm
             )
 
+        # Render all entities on the map
         # render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
         render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height,
                    bar_width, panel_height, panel_y, colors)
 
+        # Reset fov_recompute to False
         fov_recompute = False
 
         # ACtually print stuff on screen
@@ -122,6 +140,7 @@ def main():
 
         clear_all(con, entities)
 
+        # Detect player's action
         action = handle_keys(key)
 
         move = action.get('move')
@@ -130,17 +149,18 @@ def main():
 
         player_turn_results = []
 
+        # If it is player's turn and it is a move action, do it
         if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
             destination_x = player.x + dx
             destination_y = player.y + dy
 
+            
             if not game_map.is_blocked(destination_x, destination_y):
                 target = get_blocking_entities_at_location(entities, destination_x, destination_y)
 
                 # There is a blocking entity in destination tile
                 if target:
-                    # print('You kick the ' + target.name + ' in the shins, much to its annoyance!')
                     attack_results = player.fighter.attack(target)
                     player_turn_results.extend(attack_results)
 

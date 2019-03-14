de8a4e20401349264d86f89502d5c68bc6542fd7
Matteo Barbieri <neopard@gmail.com>
2019-03-14 17:52:17 +0100

    Move initialization code outside of engine.py

--- engine.py
+++ engine.py
@@ -4,6 +4,7 @@ from components.fighter import Fighter
 
 from entity import Entity, get_blocking_entities_at_location
 from input_handlers import handle_keys, handle_mouse
+from loader_functions.initialize_new_game import get_constants
 from fov_functions import initialize_fov, recompute_fov
 from render_functions import clear_all, render_all, RenderOrder
 from map_objects.game_map import GameMap
@@ -16,45 +17,7 @@ from game_messages import Message, MessageLog
 
 def main():
 
-    # Size of the game screen
-    screen_width = 80
-    screen_height = 50
-
-    # Size of the panel containing health bar and log
-    bar_width = 20
-    panel_height = 7
-    panel_y = screen_height - panel_height
-
-    # Parameters for the log panel
-    message_x = bar_width + 2
-    message_width = screen_width - bar_width - 2
-    message_height = panel_height - 1
-
-    # Size of the playing map
-    map_width = 80
-    map_height = 43
-
-    # Parameters for rooms generation
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    # Variables for field of view (FOV)
-    fov_algorithm = 0
-    fov_light_walls = True
-    fov_radius = 10
-
-    # Maximum number of monsters per room
-    max_monsters_per_room = 3
-    max_items_per_room = 2
-
-    # Colors for rooms in and out of fov
-    colors = {
-        'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
-    }
+    constants = get_constants()
 
     # Fighter component for player
     fighter_component = Fighter(hp=30, defense=2, power=5)
@@ -84,32 +47,20 @@ def main():
     )
 
     # Initialize main window
-    libtcod.console_init_root(
-        screen_width,
-        screen_height,
-        'libtcod tutorial revised',
-        False
-    )
+    libtcod.console_init_root(constants['screen_width'], constants['screen_height'], constants['window_title'], False)
 
     # The main game screen
-    con = libtcod.console_new(screen_width, screen_height)
+    con = libtcod.console_new(constants['screen_width'], constants['screen_height'])
 
     # The UI screen
-    panel = libtcod.console_new(screen_width, panel_height)
+    panel = libtcod.console_new(constants['screen_width'], constants['panel_height'])
 
     # Create the game map
-    game_map = GameMap(map_width, map_height)
-    game_map.make_map(
-        max_rooms,
-        room_min_size,
-        room_max_size,
-        map_width,
-        map_height,
-        player,
-        entities,
-        max_monsters_per_room,
-        max_items_per_room
-    )
+    game_map = GameMap(constants['map_width'], constants['map_height'])
+    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'],
+                      constants['map_width'], constants['map_height'], player, entities,
+                      constants['max_monsters_per_room'], constants['max_items_per_room'])
+
 
     # When the game starts the fov must be computed for the first time
     fov_recompute = True
@@ -117,7 +68,7 @@ def main():
     fov_map = initialize_fov(game_map)
 
     # Initialize message log
-    message_log = MessageLog(message_x, message_width, message_height)
+    message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
     # Handles for keyboard and mouse inputs
     key = libtcod.Key()
@@ -140,34 +91,13 @@ def main():
         # If the fov needs to be recomputed (for instance, because the player
         # moved, do it
         if fov_recompute:
-            recompute_fov(
-                fov_map,
-                player.x,
-                player.y,
-                fov_radius,
-                fov_light_walls,
-                fov_algorithm
-            )
+            recompute_fov(fov_map, player.x, player.y, constants['fov_radius'], constants['fov_light_walls'],
+                          constants['fov_algorithm'])
 
         # Render all entities on the map
-        render_all(
-            con,
-            panel,
-            entities,
-            player,
-            game_map,
-            fov_map,
-            fov_recompute,
-            message_log,
-            screen_width,
-            screen_height,
-            bar_width,
-            panel_height,
-            panel_y,
-            mouse,
-            colors,
-            game_state,
-            )
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log,
+                   constants['screen_width'], constants['screen_height'], constants['bar_width'],
+                   constants['panel_height'], constants['panel_y'], mouse, constants['colors'], game_state)
 
         # Reset fov_recompute to False
         fov_recompute = False
---
+++ loader_functions/initialize_new_game.py
@@ -0,0 +1,70 @@
+import libtcodpy as libtcod
+
+
+def get_constants():
+    window_title = 'Roguelike Tutorial Revised'
+
+    # Size of the game screen
+    screen_width = 80
+    screen_height = 50
+
+    # Size of the panel containing health bar and log
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
+    # Parameters for the log panel
+    message_x = bar_width + 2
+    message_width = screen_width - bar_width - 2
+    message_height = panel_height - 1
+
+    # Size of the playing map
+    map_width = 80
+    map_height = 43
+
+    # Parameters for rooms generation
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    # Variables for field of view (FOV)
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
+    # Maximum number of monsters per room
+    max_monsters_per_room = 3
+    max_items_per_room = 2
+
+    # Colors for rooms in and out of fov
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
+    }
+
+    constants = {
+        'window_title': window_title,
+        'screen_width': screen_width,
+        'screen_height': screen_height,
+        'bar_width': bar_width,
+        'panel_height': panel_height,
+        'panel_y': panel_y,
+        'message_x': message_x,
+        'message_width': message_width,
+        'message_height': message_height,
+        'map_width': map_width,
+        'map_height': map_height,
+        'room_max_size': room_max_size,
+        'room_min_size': room_min_size,
+        'max_rooms': max_rooms,
+        'fov_algorithm': fov_algorithm,
+        'fov_light_walls': fov_light_walls,
+        'fov_radius': fov_radius,
+        'max_monsters_per_room': max_monsters_per_room,
+        'max_items_per_room': max_items_per_room,
+        'colors': colors
+    }
+
+    return constants

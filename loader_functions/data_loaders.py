99e9998097cc740a54d3bc63b3883ff1dbb7b0ca
Matteo Barbieri <neopard@gmail.com>
2019-03-31 14:46:34 +0200

    Fix game save files names in .gitignore

--- loader_functions/data_loaders.py
+++ loader_functions/data_loaders.py
@@ -3,7 +3,8 @@ import os
 import shelve
 
 def save_game(player, entities, game_map, message_log, game_state):
-    with shelve.open('savegame.dat', 'n') as data_file:
+    # with shelve.open('savegame.dat', 'n') as data_file:
+    with shelve.open('savegame', 'n') as data_file:
         data_file['player_index'] = entities.index(player)
         data_file['entities'] = entities
         data_file['game_map'] = game_map

d4d71c3e50c93040a1821a280998d647d30c91c2
Matteo Barbieri <neopard@gmail.com>
2019-04-01 23:00:18 +0200

    Test simple layout

---
+++ tests/layout1.py
@@ -0,0 +1,80 @@
+import sys, os
+
+# Append parent folder
+sys.path.append(
+    os.path.join(os.path.dirname(__file__), '..'))
+
+import libtcodpy as libtcod
+
+from loader_functions.initialize_new_game import get_constants, get_game_variables
+
+
+def main():
+    constants = get_constants()
+
+    # libtcod.console_set_custom_font('data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+    libtcod.console_set_custom_font(
+        'data/fonts/brogue/font-6.png', 
+        libtcod.FONT_TYPE_GREYSCALE |
+        libtcod.FONT_LAYOUT_ASCII_INROW
+        )
+
+    screen_width = 80
+    screen_height = 30
+
+    libtcod.console_init_root(
+        screen_width, screen_height, 
+        constants['window_title'], False)
+
+    ui_left = libtcod.console_new(
+        int(screen_width/2), 
+        screen_height-10)
+    
+    ui_right = libtcod.console_new(
+        int(screen_width/2), 
+        screen_height-10)
+
+    ui_panel = libtcod.console_new(
+        int(screen_width), 
+        10)
+
+    libtcod.console_set_default_background(ui_left, libtcod.red)
+    libtcod.console_rect(ui_left, 0, 0, 40, 20, False, libtcod.BKGND_SET)
+
+    libtcod.console_set_default_background(ui_right, libtcod.blue)
+    libtcod.console_rect(ui_right, 0, 0, 40, 20, False, libtcod.BKGND_SET)
+
+    libtcod.console_set_default_background(ui_panel, libtcod.green)
+    libtcod.console_rect(ui_panel, 0, 0, 80, 10, False, libtcod.BKGND_SET)
+
+    libtcod.console_set_default_foreground(ui_left, libtcod.white)
+    libtcod.console_put_char(ui_left, 2, 2, '@', libtcod.BKGND_DEFAULT)
+
+    libtcod.console_blit(
+        ui_left, 0, 0, 
+        40, 20, 0, 
+        0, 0)
+
+    libtcod.console_blit(
+        ui_right, 0, 0, 
+        40, 20, 0, 
+        40, 0)
+
+    libtcod.console_blit(
+        ui_panel, 0, 0, 
+        80, 10, 0, 
+        0, 20)
+
+    libtcod.console_flush()
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    # Main loop
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(
+            libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
+
+
+if __name__ == '__main__':
+    main()

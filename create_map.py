97b3490d51883f8815f897019a22a31c22f4888d
Matteo Barbieri <neopard@gmail.com>
2019-04-11 11:57:26 +0200

    Add the option to manually set the seed for the RNG

--- create_map.py
+++ create_map.py
@@ -2,6 +2,9 @@
 
 import argparse
 
+import random
+import sys
+
 # from map_objects.game_map import GameMap
 from map_objects.generators.dungeon import generate_dungeon_level
 
@@ -18,12 +21,22 @@ def parse_args():
     parser.add_argument(
         '--height', type=int, default=80)
 
+    parser.add_argument(
+        '--seed', type=int)
+
 
     return parser.parse_args()
 
 def main():
 
     args = parse_args()
+
+    if args.seed is None:
+        args.seed = random.randrange(sys.maxsize)
+
+    # Initialize random number generator
+    print("Seed was:", args.seed)
+    random.seed(args.seed)
 
     newmap = generate_dungeon_level(args.width, args.height, 10, 20)
     newmap.export_txt(args.map_file)

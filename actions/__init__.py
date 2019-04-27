6db252ccde6f6e5147e4012e4f7139afc9e38dc9
Matteo Barbieri <neopard@gmail.com>
2019-04-27 17:51:54 +0200

    Rename actions/show_menu.py -> actions/menus.py

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,5 +1,5 @@
 from .move import MoveAction
-from .show_menu import ShowMenuAction
+from .menus import ShowMenuAction
 from .action import NoopAction, WaitAction
 from .toggle_fullscreen import ToggleFullscreenAction
 from .exceptions import ShowMenuException
